e6d8bd2f5ee361497d6b1c1499c01a04adcbd013
Damien Krotkine <damien.krotkine@alan.eu>
2022-12-01 09:12:59 +0100

    day1

---
+++ day_1/part1.exs
@@ -0,0 +1,10 @@
+File.read!("input")
+|> String.split("\n")
+|> Enum.chunk_by(& &1 == "")
+|> Enum.reject(& &1 == [""])
+|> Enum.map(fn l -> l
+  |> Enum.map(&String.to_integer/1)
+  |> Enum.sum()
+end)
+|> Enum.max()
+|> IO.inspect()
