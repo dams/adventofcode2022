e543f58793e0ecc0bc1612f2dd741137d3875e23
Damien Krotkine <damien.krotkine@alan.eu>
2022-12-05 22:17:39 +0100

    simplofy

--- day_4/solution.exs
+++ day_4/solution.exs
@@ -1,18 +1,12 @@
 File.stream!("input")
 |> Enum.reduce({0,0}, fn line, {acc_part1, acc_part2} ->
     [s1, e1, s2, e2] = line
-    |> String.split(~r/\D/, trim: true)
-    |> Enum.map(&String.to_integer/1)
+      |> String.split(~r/\D/, trim: true)
+      |> Enum.map(&String.to_integer/1)
+    {m1, m2} = {MapSet.new(s1..e1), MapSet.new(s2..e2)}
     {
-      acc_part1 + cond do
-         s1 <= s2 && e1 >= e2 -> 1
-         s2 <= s1 && e2 >= e1 -> 1
-         true -> 0
-      end,
-      acc_part2 + cond do
-          Range.disjoint?(s1..e1, s2..e2) -> 0
-          true -> 1
-      end
+      acc_part1 + (if MapSet.subset?(m1,m2) || MapSet.subset?(m2,m1), do: 1, else: 0),
+      acc_part2 + (if MapSet.disjoint?(m1,m2), do: 0, else: 1),
     }
 end)
 |> IO.inspect()
